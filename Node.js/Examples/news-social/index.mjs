ce6bf4ad9c1386a67ba8763c63dc6283bf402f2d
VaniaDinCahul <caliniacovlev@gmail.com>
2024-04-30 19:21:51 +0300

    30.04.2024

--- Node.js/Examples/news-social/index.mjs
+++ Node.js/Examples/news-social/index.mjs
@@ -1,19 +1,23 @@
 import { news } from "./data.mjs";
 import { renderNews } from "./ui.mjs";
 // 1
-import readFile from 'node:fs';
+// import readFile from 'node:fs';
 // 2
-// import readFile from "node:fs/promises";
-
+import readFile from "node:fs/promises";
+import writeFile from "node:fs/promises";
 // 1
-readFile('./news.txt', (err, data) => {
-    console.log(data.toString())
-})
+// readFile('./news.txt', (err, data) => {
+//     console.log(data.toString())
+// })
 
 // 2
+let title = "yes"
 
-// let data = await readFile('./news.txt')
-// console.log(data.toString());
+await writeFile('./news.txt', title)
+
+let data = await readFile('./news.json')
+data = JSON.parse(data)
+console.log(data.toString());
 
 
 renderNews(news)
