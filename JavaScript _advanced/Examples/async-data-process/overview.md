67c958f814ef43daa0d4916dee731f50705fa241
VaniaDinCahul <caliniacovlev@gmail.com>
2024-05-10 19:32:35 +0300

    10.05.2024 | Cleanup

--- JavaScript _advanced/Examples/async-data-process/overview.md
+++
@@ -1,3 +0,0 @@
-# #getData ---> processData --> displayData
-
-
--- Website/src/js/script.js
+++
@@ -1,7 +0,0 @@
-new Product(
-    'Produs1',
-    'Fabrticat in germanie in anul 1742',
-    '799.99',
-    '699.99', 
-    'nil',
-    ['electric', 'istoric']);
