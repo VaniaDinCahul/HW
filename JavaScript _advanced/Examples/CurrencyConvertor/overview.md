4001eadca4f71299e7c9751c32bfbfbef3bac63d
VaniaDinCahul <caliniacovlev@gmail.com>
2024-06-06 22:23:58 +0300

    06.06.2024 | Cleanup

--- JavaScript _advanced/Examples/CurrencyConvertor/overview.md
+++
@@ -1,1 +0,0 @@
-# JS / Currency convertor / API
--- Node.js/Examples/Modular_app/author.mjs
+++
@@ -1,6 +0,0 @@
-// Module 1
-let name        = "Dorin E."
-let email       = "doring.e.art@gmail.com"
-let secretCode  = "1234"
-
-export {name, email}
